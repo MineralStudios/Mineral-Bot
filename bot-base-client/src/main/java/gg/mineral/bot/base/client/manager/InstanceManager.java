2356332a8e56a93a60ca16c80a6742b5029eee24
Jaiden <48765522+jaidennnnn@users.noreply.github.com>
2024-09-23 11:18:36 +0100

    Test out concurrenthashmap in instance manager

--- bot-base-client/src/main/java/gg/mineral/bot/base/client/manager/InstanceManager.java
+++ bot-base-client/src/main/java/gg/mineral/bot/base/client/manager/InstanceManager.java
@@ -3,12 +3,12 @@ package gg.mineral.bot.base.client.manager;
 import java.util.UUID;
 
 import gg.mineral.bot.base.client.player.FakePlayerInstance;
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import lombok.Getter;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class InstanceManager {
     @Getter
-    private static final Object2ObjectOpenHashMap<UUID, FakePlayerInstance> instances = new Object2ObjectOpenHashMap<>();
+    private static final ConcurrentHashMap<UUID, FakePlayerInstance> instances = new ConcurrentHashMap<>();
 
     public static boolean isRunning() {
         return !instances.isEmpty();
